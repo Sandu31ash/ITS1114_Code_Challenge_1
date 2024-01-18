65d20a29cbaf24a4a3ab66bc6db8902509613499
Sanduni253127 <hazinika253127@gmail.com>
2024-01-18 21:48:02 +0530

    implement customer save

--- src/main/java/lk/ijse/its1114_code_challenge_01/api/Item.java
+++ src/main/java/lk/ijse/its1114_code_challenge_01/api/Item.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,6 +19,15 @@ import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+
+@WebServlet(name = "item",urlPatterns = "/item"
+//        initParams = {
+//                @WebInitParam(name = "db-user",value = "root"),
+//                @WebInitParam(name = "db-pw",value = "mysql"),
+//                @WebInitParam(name = "db-url",value = "jdbc:mysql://localhost:3306/gdse65JavaEE?createDatabaseIfNotExist=true"),
+//                @WebInitParam(name = "db-class",value = "com.mysql.cj.jdbc.Driver")
+//        }
+)
 
 public class Item extends HttpServlet {
     final static Logger logger = LoggerFactory.getLogger(Item.class);
@@ -40,24 +50,6 @@ public class Item extends HttpServlet {
             e.printStackTrace();
         }
     }
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, IOException {
-        if(req.getContentType() == null ||
-                !req.getContentType().toLowerCase().startsWith("application/json")){
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
-        }else {
-            Jsonb jsonb = JsonbBuilder.create();
-            var itemDTO = jsonb.fromJson(req.getReader(), ItemDTO.class);
-            var dbProcess = new DBProcess();
-            dbProcess.saveItemOne(itemDTO,connection);
 
-//            List<ItemDTO> itemList= jsonb.fromJson(req.getReader(),new ArrayList<ItemDTO>(){
-//            }.getClass().getGenericSuperclass());
-//            var dbProcess = new DBProcess();
-//            dbProcess.saveItem(itemList,connection);
-//            //itemList.forEach(System.out::println);
-//            jsonb.toJson(itemList,resp.getWriter());
-        }
 
-    }
 }
--- src/main/java/lk/ijse/its1114_code_challenge_01/db/DBProcess.java
+++ src/main/java/lk/ijse/its1114_code_challenge_01/db/DBProcess.java
@@ -1,5 +1,7 @@
 package lk.ijse.its1114_code_challenge_01.db;
 
+//import lk.ijse.its1114_code_challenge_01.dto.CustomerDTO;
+import lk.ijse.its1114_code_challenge_01.dto.CustomerDTO;
 import lk.ijse.its1114_code_challenge_01.dto.ItemDTO;
 //import lombok.var;
 import org.slf4j.Logger;
@@ -13,26 +15,28 @@ import java.util.UUID;
 
 public class DBProcess {
 
-    private static final String SAVE_DATA = "INSERT INTO customer (ID,NAME,CONTACT,ADDRESS) VALUES (?,?,?,?)";
-    private static final String GET_DATA = "SELECT * FROM customer WHERE id = ?";
+    private static final String SAVE_CUSTOMER_DATA = "INSERT INTO customer (ID,NAME,CONTACT,ADDRESS) VALUES (?,?,?,?)";
+    private static final String GET_CUSTOMER_DATA = "SELECT * FROM customer WHERE id = ?";
 
     final static Logger logger = LoggerFactory.getLogger(DBProcess.class);
 
-    private static final String SAVE_ITEM_DATA = "INSERT INTO ItemNew (CODE,DES,PRICE,QTY) VALUES (?,?,?,?)";
+    private static final String SAVE_ITEM_DATA = "INSERT INTO item (CODE,DES,PRICE,QTY) VALUES (?,?,?,?)";
 
-    public String saveCustomerData(String id, String name, String contact, String address, Connection connection) {
-        // save / manipulate data
+    public void saveCustomerData(CustomerDTO customerDTO, Connection connection){
+//        String customItemId = "IT "+UUID.randomUUID();
         try {
-            var ps = connection.prepareStatement(SAVE_DATA);
-            ps.setString(1, id);
-            ps.setString(2, name);
-            ps.setString(3, contact);
-            ps.setString(4, address);
+            var ps = connection.prepareStatement(SAVE_CUSTOMER_DATA);
+            ps.setString(1, customerDTO.getId());
+            ps.setString(2, customerDTO.getName());
+            ps.setString(3, customerDTO.getContact());
+            ps.setString(4, customerDTO.getAddress());
 
             if (ps.executeUpdate() != 0) {
-                return "Data saved";
+                logger.info("Data saved");
+                System.out.println("Data saved");
             } else {
-                return "Failed to save data";
+                logger.error("Failed to save");
+                System.out.println("Failed to save");
             }
 
         } catch (SQLException e) {
@@ -40,77 +44,5 @@ public class DBProcess {
         }
     }
 
-    public List<String> getCustomerData(String id, Connection connection) {
-        //get data
-        List<String> selectedCustomer = new ArrayList<>();
-        try {
-            var ps = connection.prepareStatement(GET_DATA);
-            ps.setInt(1, Integer.parseInt(id));
-            var rs = ps.executeQuery();
-
-            while (rs.next()) {
-                int custId = rs.getInt("id");
-                String name = rs.getString("name");
-                String city = rs.getString("city");
-                String email = rs.getString("email");
-
-                selectedCustomer.add(String.valueOf(custId));
-                selectedCustomer.add(name);
-                selectedCustomer.add(city);
-                selectedCustomer.add(email);
-            }
-            return selectedCustomer;
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void saveItem(List<ItemDTO> items,Connection connection){
-          String customItemId = "IT "+UUID.randomUUID();
-            for(ItemDTO itemData : items){
-                try {
-                    var ps = connection.prepareStatement(SAVE_ITEM_DATA);
-                    ps.setString(1, customItemId);
-                    ps.setString(2, String.valueOf(itemData.getDes()));
-                    ps.setDouble(3, itemData.getPrice());
-                    ps.setInt(4, itemData.getQty());
-
-                    if (ps.executeUpdate() != 0) {
-
-                        System.out.println("Data saved");
-                    } else {
-
-                        System.out.println("Failed to save");
-
-                    }
-
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-    }
-
-    public void saveItemOne(ItemDTO items, Connection connection){
-        String customItemId = "IT "+UUID.randomUUID();
-            try {
-                var ps = connection.prepareStatement(SAVE_ITEM_DATA);
-                ps.setString(1, customItemId);
-                ps.setString(2, String.valueOf(items.getDes()));
-                ps.setDouble(3, items.getPrice());
-                ps.setInt(4, items.getQty());
-
-                if (ps.executeUpdate() != 0) {
-                    logger.info("Data saved");
-                    System.out.println("Data saved");
-                } else {
-                    logger.error("Failed to save");
-                    System.out.println("Failed to save");
-                }
-
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
 
 }
